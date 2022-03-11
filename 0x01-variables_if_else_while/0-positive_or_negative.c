847e71eacfffa97e5a428060dbbb7da4f3f0e360
Jerome Adorkor <jadorkor27@gmail.com>
2022-03-11 15:07:47 -0800

    Fixed betty styling

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -2,6 +2,12 @@
 #include <time.h>
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
 int main(void)
 {
 	int n;
@@ -10,12 +16,11 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 
-	if(n < 0)
+	if (n < 0)
 		printf("%d is negative\n", n);
 	else if (n == 0)
 		printf("%d is zero\n", n);
 	else
 		printf("%d is positive\n", n);
-	
 	return (0);
 }
