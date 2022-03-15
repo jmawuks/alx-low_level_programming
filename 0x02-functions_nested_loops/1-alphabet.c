cd278a667e36aa228c0536c36b01a96d15dca975
Jerome Adorkor <jadorkor27@gmail.com>
2022-03-15 16:08:00 -0700

    Testing 1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ *Return: Always 0.
+ */
+
+void print_alphabet(void)
+{
+	char alphabets[27] = "abcdefghijklmnopqrstuvwxyz\n";
+
+	int i;
+
+	for (i = 0; i < 10; i++)
+		putchar(alphabets[i]);
+	return;
+}
