8a7ae14897783f2bedd0c87bd37d67b22a1547c1
Jerome Adorkor <jadorkor27@gmail.com>
2022-03-15 17:41:28 -0700

    Added script to find abs of a number

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _abs - finds absolute of a number
+ * @int: number we try to find abs of
+ *
+ * Return: int +, -
+ */
+
+int _abs(int)
+{
+	int value;
+
+	return (abs(value));
+}
+
