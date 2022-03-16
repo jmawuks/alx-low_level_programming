fc513cb756c75b1c5015b0fc6b3196b546a2daeb
Jerome Adorkor <jadorkor27@gmail.com>
2022-03-15 17:00:35 -0700

    Added script to check if letters are lowercase

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_alphabet - prints small letters of all alphabets
+ *
+ * Return: void
+ */
+
+int _islower(int c)
+{
+	if (ch >= 97 && ch <= 122)
+		c = 1;
+	else
+		c = 0;
+
+	return c;
+}
+
