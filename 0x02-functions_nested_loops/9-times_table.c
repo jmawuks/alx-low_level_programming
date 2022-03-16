1e928073b85632ebd7c13622e0c610e8f4c2c3e2
Jerome Adorkor <jadorkor27@gmail.com>
2022-03-15 19:44:26 -0700

    Added script for the 9times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -2,31 +2,9 @@
 #include <stdio.h>
 
 /**
- * add_space - adds a comma and a space for numbers larger than 8
- *
- */
-
-/**
- * add_space_less10 - adds a comma and two spaces for numbers smaller than 10
- */
-
-/**
  * times_table - prints the 9 times table from 0
  *
-*/
-
-void add_space(void)
-{
-	_putchar (',');
-	_putchar (' ');
-}
-
-void add_space_less10(void)
-{
-	_putchar (',');
-	_putchar (' ');
-	_putchar (' ');
-}
+ */
 
 void times_table(void)
 {
@@ -59,9 +37,16 @@ void times_table(void)
 			if (j < 10)
 			{
 				if (nextResult >= 10)
-					add_space();
+				{
+					_putchar (',');
+					_putchar (' ');
+				}
 				else if (nextResult < 10)
-					add_space_less10();
+				{
+					_putchar (',');
+					_putchar (' ');
+					_putchar (' ');
+				}
 			}
 		}
 
