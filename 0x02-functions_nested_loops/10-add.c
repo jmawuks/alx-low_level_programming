f5578d5650c944730d76a5969139c1480c5691e3
Jerome Adorkor <jadorkor27@gmail.com>
2022-03-15 19:49:14 -0700

    Added script to add two numbers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * add - adds two numbers
+ * @o: first number to add
+ * @j: second number to add
+ *
+ * Return: int value
+ */
+
+int add(int o, int j)
+{
+	int value;
+
+	value = o + j
+
+	return (value);
+}
+
