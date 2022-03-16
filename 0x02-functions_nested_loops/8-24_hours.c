9be54984c1b554649900218305333f446a0b12f4
Jerome Adorkor <jadorkor27@gmail.com>
2022-03-15 18:21:22 -0700

    Added script to print every minute of 24 hours

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,51 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * jack_bauer - prints every minute of a whole day
+ *
+ * Return: 0
+ */
+
+void jack_bauer(void)
+{
+	int h1;
+	int h2;
+	int m1;
+	int m2;
+
+	h1 = 0;
+	h2 = 0;
+	m1 = 0;
+	m2 = 0;
+
+	while (h1 < 2)
+	{
+		while (h2 < 9)
+		{
+			while (m1 < 5)
+			{
+				while (m2 < 9)
+				{
+					_putchar ('0' + h1);
+					_putchar ('0' + h2);
+					_putchar (':');
+					_putchar ('0' + m1);
+					_putchar ('0' + m2);
+					m2++;
+				}
+
+				m1++;
+				m2 = 0;
+			}
+
+			h2++;
+			m1 = 0;
+
+		}
+
+		h1++;
+		h2 = 0;
+	}
+}
+
