2f869d2d1897b9887e4f6f407375af24cf87281f
Jerome Adorkor <jadorkor27@gmail.com>
2022-03-17 09:15:19 -0700

    Initialised project and added main.c main.h and script to print if a number is positive or negative

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * positive_or_negative - Determines if a number is negative or positive
+ * @n - inputted number
+ *
+ * Return: Always 0 (Success)
+ */
+
+void positive_or_negative(int n)
+{	
+	if (n < 0)
+		printf("%d is negative\n", n);
+
+	else if (n == 0)
+		printf("%d is zero\n", n);
+
+	else
+		printf("%d is positive\n", n);
+}
