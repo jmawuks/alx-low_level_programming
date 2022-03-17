0c246f4bbaed1664da4e9b86cfd518e54b199cc9
Jerome Adorkor <jadorkor27@gmail.com>
2022-03-17 09:55:18 -0700

    Added equal check for numbers

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -12,17 +12,24 @@ int largest_number(int a, int b, int c)
 {
 	int largest;
 
-	if ((a > b && b > c) || (a > c && c > b))
+	if ((a > b && b > c) || (a > c && c > b) ||
+			(a == b && b > c) || (a == c && c > b))
 	{
 		largest = a;
 	}
-	else if ((b > a && a > c) || (b > c && c > a))
+	else if ((b > a && a > c) || (b > c && c > a)
+			|| (b == a && a > c) || (b == c && c > a))
 	{
 		largest = b;
 	}
-	else if ((c > a && a > b) || (c > b && b > a))
+	else if ((c > a && a > b) || (c > b && b > a)
+			|| (c == a && a > b) || (c == b && c > a))
 	{
 		largest = c;
+	}
+	else
+	{
+		largest = a;
 	}
 
 	return (largest);
