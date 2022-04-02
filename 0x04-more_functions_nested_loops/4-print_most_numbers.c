b205ef2dfc048436ddcf8439b4d8ee5d1e313042
Jerome Adorkor <jadorkor27@gmail.com>
2022-04-02 07:13:55 -0700

    Added script to print most numbers

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_most_numbers - prints all digit from 0-9 except 2 and 4
+ *
+ * Return: void
+ */
+
+void print_numbers(void)
+{
+	char digits[9] = "01356789\n";
+
+	int i;
+
+	for (i = 0; i < 9; i++)
+		_putchar(digits[i]);
+}
