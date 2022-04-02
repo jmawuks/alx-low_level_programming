3c76bc089d2370ebf0aa85e814faa660c96a9a8e
Jerome Adorkor <jadorkor27@gmail.com>
2022-04-02 14:48:21 -0700

    Added script to print last prime number of 612852475143

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - prints prime factors of 612852475143
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	long int number, count;
+	number = 612852475143;
+
+	for (count = 2; number > 1; count++)
+	{
+		while (number % count == 0)
+		{
+			number = number / count;
+		}
+	}
+
+	printf("%ld ", count);
+	printf("\n");
+
+	return (0);
+}
