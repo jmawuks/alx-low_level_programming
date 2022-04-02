6bda7ea3480ce21092885854b0e820d1364ee12d
Jerome Adorkor <jadorkor27@gmail.com>
2022-04-02 07:28:06 -0700

    Added script to print lines based on number inputted

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_line - prints an underscore based on n
+ * @n: number of times the underscire should print
+ *
+ * Return: void
+ */
+
+void print_line(int n)
+{
+	int i;
+
+	if (n > 0)
+	{
+		for (i = 0; i < n; i++)
+			_putchar('_');
+	}
+
+	_putchar('\n');
+}
