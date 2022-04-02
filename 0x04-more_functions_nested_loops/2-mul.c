a43ce351cbfda029bf32c0a428f2dbeeeba8dc6e
Jerome Adorkor <jadorkor27@gmail.com>
2022-04-02 06:54:05 -0700

    Added script to multiply two numbers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * mul - multiplies to numbers
+ * @a: first number
+ * @b: second number
+ *
+ * Return: int product
+ */
+
+int mul(int a, int b)
+{
+	int product;
+
+	product = a * b;
+
+	return (product);
+}
