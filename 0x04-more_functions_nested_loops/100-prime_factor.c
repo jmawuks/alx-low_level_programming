ae78479159ed8d4887667bd6617ecbef5964a09c
Jerome Adorkor <jadorkor27@gmail.com>
2022-04-02 15:01:41 -0700

    Fixed betty coding style

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -15,13 +15,13 @@ int main(void)
 
 	n = 612852475143;
 	max = -1;
-	
+
 	while (n % 2 == 0)
 	{
 		max = 2;
 		n /= 2;
 	}
-	
+
 	for (i = 3; i <= sqrt(n); i = i + 2)
 	{
 		while (n % i == 0)
@@ -33,7 +33,7 @@ int main(void)
 
 	if (n > 2)
 		max = n;
-	
+
 	printf("%ld\n", max);
 	return (0);
 }
